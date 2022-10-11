3689f824afa25a3a03d11013376be5e096b29740
Kaushik Chemburkar <attherate@gmail.com>
2022-10-11 20:37:02 +1100

    🪒 Scraped hackernews

--- p10_news_scraper.py
+++ p10_news_scraper.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 
-website = "https://www.thesun.co.uk/sport/football/"
+website = "https://news.ycombinator.com/news"
 path = "/usr/local/bin/chromedriver"
 
 service = Service(executable_path=path)
@@ -10,19 +10,19 @@ driver = webdriver.Chrome(service=service)
 driver.get(website)
 
 # Heading: //div[@class="css-co4v5n"]
-containers = driver.find_elements(by="xpath", value='//div[@class="css-co4v5n"]')
+containers = driver.find_elements(by="xpath", value='//tr[@class="athing"]')
 
 titles = []
 subtitles = []
 
 for container in containers:
-    title = container.find_element(by="xpath", value='./h3').text
-    subtitle = container.find_element(by="xpath", value='./section/h2/span').text
+    title = container.find_element(by="xpath", value='./td/span').text
+    subtitle = container.find_element(by="xpath", value='./td/span/a').text
     titles.append(title)
     subtitles.append(subtitle)
 
-my_dict = {'titles': titles, 'subtitles': subtitles}
+my_dict = {'Number': titles, 'Headline': subtitles}
 df_headlines = pd.DataFrame(my_dict)
-df_headlines.to_csv('headlines.csv', index=False)
+df_headlines.to_csv('hackernews.csv', index=False)
 
 driver.quit()
