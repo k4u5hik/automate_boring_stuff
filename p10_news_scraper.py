62b696701c023505dc5088957a75c86fa4e2cf7c
Kaushik Chemburkar <attherate@gmail.com>
2022-10-12 13:51:30 +1100

    🚨 Compile Error✅  Resolved-Reinstalled sqlalchemy

--- p10_news_scraper.py
+++ p10_news_scraper.py
@@ -2,6 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
+from datetime import datetime
+import os
+import sys
+
+# get the path of the executable that we will create. we will export the file in the same folder where the executable is
+application_path = os.path.dirname(sys.executable)
+# datetime will be used to customise the name of the filename
+now = datetime.now()
+month_day_year = now.strftime("%d-%m-%Y-%H:%M:%S") #string format of the current date and time. Search Python strftime cheatsheet.
 
 website = "https://news.ycombinator.com/news"
 path = "/usr/local/bin/chromedriver" # was getting permissions error without this path, it worked with the path
@@ -29,8 +38,14 @@ for container in containers:
     subtitles.append(subtitle)
     # links.append(link)
 
-my_dict = {'Number': titles, 'Headline': subtitles} # 'Link': links}
+my_dict = {'Number': titles, 'Headline': subtitles} # 'Link': links
 df_headlines = pd.DataFrame(my_dict)
-df_headlines.to_csv('hackernews-headless.csv', index=False)
+
+file_name = f'hackernews-headless-date-{month_day_year}.csv'
+final_path = os.path.join(application_path, file_name) # join the path of the executable with the name of the file
+df_headlines.to_csv(final_path) # customise the name of the file
+# pip install pyinstaller
+# pyinstaller --onefile p10_news_scraper.py
+# SyntaxError: multiple exception types must be parenthesized - Had this bug due to SQLAlchemy. Had to uninstall and reinstall SQLAlchemy. Resolved.
 
 driver.quit()
