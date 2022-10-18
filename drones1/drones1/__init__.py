f4cbf3a6f8a00539bee8674b581d5679b8445021
Kaushik Chemburkar <attherate@gmail.com>
2022-10-18 19:00:19 +1100

    ❌ MemoryError: Cannot allocate write+execute memory for ffi.callback().
    https://github.com/pyca/pyopenssl/issues/873
    
    - other website with http protocol worked.

---
+++ drones1/drones1/__init__.py
@@ -0,0 +1,2 @@
+# https://www.youtube.com/watch?v=hCARQVJy_mk&list=PLRzwgpycm-Fi5Pe_W2HwEwyvcB5-SJLB7&index=2
+# Getting memory error, abandoning for now because this can be done with selenium anyway
